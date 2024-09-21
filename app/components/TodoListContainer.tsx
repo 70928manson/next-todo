1677696c24d61440ecef167efcdf6a8e77f7e002
Manson <manson972@yahoo.com>
2024-09-21 13:45:31 +0800

    modify: add loading spiner and update props for TodoItem, TodoPagination

--- app/components/TodoListContainer.tsx
+++ app/components/TodoListContainer.tsx
@@ -3,72 +3,60 @@
 import React from 'react'
 import { TodoItem } from './TodoItem';
 
-import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from "@/components/ui/pagination"
+import { TodoPagination } from './TodoPagination';
+
+import ClipLoader from "react-spinners/ClipLoader";
+import { Todo } from '../types/todo';
 
 interface TodoListContainerProps {
     todos: Todo[],
-    getTodo: () => void
+    todoType: string,
+    total: number,
+    getTodo: () => void,
+    currentPage: number,
+    setCurrentPage: React.Dispatch<React.SetStateAction<number>>,
+    loading: boolean
 }
 
-type Todo = {
-    id: number;
-    name: string;
-    description: string;
-    is_completed: boolean;
-    created_at: string;
-    updated_at: string;
-}
-
-export const TodoListContainer = ({ todos, getTodo }: TodoListContainerProps) => {
-    // const [page, setPage] = useState(1);
+export const TodoListContainer = ({ todos, todoType, total, getTodo, currentPage, setCurrentPage, loading }: TodoListContainerProps) => {
 
     return (
         <div className="p-6 bg-[#333333] rounded flex flex-col gap-4">
+            {
+                loading && (
+                    <div className="flex justify-center items-center">
+                        <ClipLoader
+                            color="#ffffff"
+                            loading={loading}
+                            size={25}
+                            aria-label="Loading Spinner"
+                            data-testid="loader"
+                        />
+                    </div>
+                )
+            }
             {
                 todos.map((todo) => {
                     return (
                         <TodoItem
                             key={todo.id}
-                            id={todo.id}
-                            name={todo.name}
-                            description={todo.description}
-                            is_completed={todo.is_completed}
-                            created_at={todo.created_at}
-                            updated_at={todo.updated_at}
+                            todo={todo}
                             getTodo={getTodo}
+                            type={todoType}
+                            currentPage={currentPage}
+                            total={total}
+                            todoType={todoType}
                         />
                     )
                 })
             }
-            {/*TODO: 分頁 */}
-            <Pagination>
-                <PaginationContent>
-                    onclick page -1
-                    <PaginationItem>
-                        <PaginationPrevious href="#" />
-                    </PaginationItem>
-
-                    頁數
-                    <PaginationItem>
-                        <PaginationLink href="#">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                    onclick page +1
-                    <PaginationItem>
-                        <PaginationNext href="#" />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+            <TodoPagination
+                type={todoType}
+                total={total}
+                getTodo={getTodo}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
         </div>
     )
 }
